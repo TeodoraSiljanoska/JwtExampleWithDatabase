d4306ac0042a5c0abc4962790b9dabd73b5b03ea
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-07-17 16:29:56 +0200

    Middleware folder and AppSettings.cs model deleted, because they weren't used

--- JwtExampleWithDatabase/Models/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿/* namespace JwtExampleWithDatabase.Models
-{
-    public class AppSettings
-    {
-        //public string Secret = "ZrQLvQskGn335pe7gR5qZrQLvQskGn335pe7gR5qZrQLvQskGn335pe7gR5q";
-        public string Secret = "E6Hl2zghrb7DpnkzTEc2E6Hl2zghrb7DpnkzTEc2";
-        // public string Secret { get; set; }
-    }
-} */
